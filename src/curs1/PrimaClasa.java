550c780e9f4799895c97ced2bc4d7b4226bdfa3e
Andra Andra <andra_ahmed@yahoo.com>
2023-09-12 12:43:55 +0300

    commit message

---
+++ src/curs1/PrimaClasa.java
@@ -0,0 +1,13 @@
+package curs1;
+
+public class PrimaClasa {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		PrimaClasa obiect = new PrimaClasa();
+		
+		
+
+	}
+}
