73dcb835e2d694a4d22ee430726b567411539d8e
Andra Andra <andra_ahmed@yahoo.com>
2023-09-12 17:32:53 +0300

    commit PrimaClasa

--- src/curs1/PrimaClasa.java
+++ src/curs1/PrimaClasa.java
@@ -6,8 +6,26 @@ public class PrimaClasa {
 		System.out.println("Hello");
 		
 		PrimaClasa obiect = new PrimaClasa();
+		obiect.sendMessage();
 		
+		System.out.println(obiect.afiseazaSuma());
+		//System.out.println(obiect.sendMessage());
 		
+		int numar = 10 + obiect.afiseazaSuma();
+		System.out.println(numar);
 
 	}
+	public void sendMessage() {
+		System.out.println("Message");
+		
+	}
+	
+	public int afiseazaSuma() {
+		
+		int suma = 20+30;
+		return suma;
+	}
+	
+	
+	
 }
